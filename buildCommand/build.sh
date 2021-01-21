c5d6d1894af54f9ab9fe3030666121eb0e9a7483
liping3 <liping3@chehejia.com>
2021-01-21 14:55:43 +0800

    add FuncBlockCount pass

--- buildCommand/build.sh
+++ buildCommand/build.sh
@@ -2,3 +2,5 @@
 cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=~/bin/llvm_s ../llvm
 # run pass
 ./bin/opt -load lib/LLVMHello.so  -hello test.bc
+# gen ll
+clang -emit-llvm -S test.mm -o test.ll
--- pass/FuncBlockCount/FuncBlockCount.cpp
+++ pass/FuncBlockCount/FuncBlockCount.cpp
@@ -1,6 +1,7 @@
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Analysis/LoopInfo.h"
 
 using namespace llvm;
 
@@ -8,9 +9,32 @@ namespace {
 	struct FuncBlockCount : public FunctionPass {
 		static char ID;
 		FuncBlockCount():FunctionPass(ID) {}
+
+		void countBlockInLoop(Loop *L, unsigned nest) {
+			unsigned numBlocks = 0;
+			Loop::block_iterator blkIter;
+			for(blkIter=L->block_begin();blkIter!=L->block_end();++blkIter){
+				numBlocks++;
+			}
+			errs()<<"Loop level"<<nest<<" has "<<numBlocks<<" blocks\n";
+			std::vector<Loop*> subLoops = L->getSubLoops();
+			Loop::iterator j, f;
+			for(j=subLoops.begin(),f=subLoops.end();j!=f;++j)
+				countBlockInLoop(*j, nest+1);
+			
+		}
+
 		bool runOnFunction(Function &F) override {
-			errs()<<"Function"<<F.getName()<<'\n';
+			errs()<<"Function: "<<F.getName()<<'\n';
+			LoopInfo &LI = getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
+			auto Loops = LI.getLoopsInPreorder();
+			for(Loop *L:Loops)
+				countBlockInLoop(L, 0);
 			return false;
+		}
+
+		virtual void getAnalysisUsage(AnalysisUsage& AU) const override {
+			AU.addRequired<LoopInfoWrapperPass>();
 		}
 	};
 }
